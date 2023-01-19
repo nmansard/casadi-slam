9e61b86091d0836022844dfaf207097d0154e45e
Joan Solà <jsola@iri.upc.edu>
2023-01-19 20:29:43 +0100

    move cost fncns to external file

--- slam.py
+++ slam.py
@@ -16,50 +16,7 @@ from gslam_april_tools import *
 #-----------------------------------------------------------------------------------
 # COST FUNCTIONS
 #-----------------------------------------------------------------------------------
-
-# motion -- constant position
-def cost_constant_position(sqrt_info, keyframe_i, keyframe_j):
-    ppred = keyframe_i.R.T @ (keyframe_j.position - keyframe_i.position)
-    wpred = log3(exp3(-keyframe_i.anglevector) @ exp3(keyframe_j.anglevector)) # log ( Ri.T * Rj )
-    pred  = casadi.vertcat( ppred, wpred )
-
-    cost = casadi.sumsqr(sqrt_info @ pred) 
-    return cost
-
-# landmark observations
-def cost_landmark(meas, sqrt_info, keyframe_i, landmark_j):
-    # compose: landmark pose wrt KF pose
-    ppred = keyframe_i.R.T @ (landmark_j.position - keyframe_i.position)
-    Rpred = keyframe_i.R.T @ landmark_j.R
-
-    # error: use manifold tools for the orientation part
-    perr = meas[0:3] - ppred
-    Rerr = Rpred.T @ exp3(meas[3:6])
-    werr = log3(Rerr)
-
-    err = casadi.vertcat(perr, werr)
- 
-    # apply weight and compute squared norm
-    cost = casadi.sumsqr(sqrt_info @ err) 
-    return cost
-
-# Prior on first KF
-def cost_keyframe_prior(meas, sqrt_info, keyframe):
-    perr = meas[0:3] - keyframe.position
-    werr = log3(keyframe.R.T @ exp3(meas[3:6]))
-    err  = casadi.vertcat( perr, werr )
-
-    cost = casadi.sumsqr(sqrt_info @ err) 
-    return cost
-
-# Prior on one landmark
-def cost_landmark_prior(meas, sqrt_info, landmark):
-    perr = meas[0:3] - landmark.position
-    werr = log3(landmark.R.T @ exp3(meas[3:6]))
-    err  = casadi.vertcat( perr, werr )
-
-    cost = casadi.sumsqr(sqrt_info @ err) 
-    return cost
+from cost_functions import *
 
 
 def computeTotalCost(factors, keyframes, landmarks):
@@ -119,7 +76,7 @@ viz = MeshcatVisualizer()
 
 # time and ID counters
 t        = 0
-t_period = 2
+t_period = 1
 kf_id    = 0
 fac_id   = 0
 
@@ -145,7 +102,7 @@ print_detection_metrics = False
 # process all images in the sequence
 prior_is_set    = False
 
-while(t <= 250):
+while(t <= 100):
 
     # read one image
     filename = file_tmp.format(num=t)
